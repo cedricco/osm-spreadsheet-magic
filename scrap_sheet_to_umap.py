41cf5e4dae396041fc3c764d0bd2ee62065b88ee
Cédric Codet <cedric.codet@polyconseil.fr>
2020-12-10 17:57:17 +0100

    test with distinct features

--- scrap_sheet_to_umap.py
+++ scrap_sheet_to_umap.py
@@ -21,7 +21,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SAMPLE_SPREADSHEET_ID = '1xITn4DPQDIlT_qiO51JpM_e7PiHQIZkX0zygcE3AWGY'
 SAMPLE_RANGE_NAME = 'Alternatives!A1:AB'
 COLUMN_NAMES = ['', 'Région', 'Département', 'alternative', 'Commune', """Adresse postale 
-⚠️ + latitude/longitude si PB affichage carte (passer souris pour + d'infos)""", 'Initié par...          (prénom : date)', 'source', 'Contacté par...          (prénom : date)\nRelance le...', 'retour', 'Thèmatique', 'Activité', 'date de passage', 'Prénom', 'Nom',
+⚠️ + latitude/longitude si PB affichage carte (passer souris pour + d'infos)""", 'Initié par...          (prénom : date)', 'source', 'Contacté par...          (prénom : date)\nRelance le...', 'retour', 'Thèmatique', 'descriptif', 'date de passage', 'Prénom', 'Nom',
                 'Mail', 'Téléphone', 'Portable', 'site internet', 'Réseaux sociaux', 'Rédacteur recueil', 'Carto plénière: incontournable ?', 'Carto plénière: punchline 1', 'Carto plénière: punchline 2', 'Carto plénière: punchline 3', 'Carto plénière: hashtag', 'Carto plénière: classification', 'Carto plénière: image']
 
 DAYS_3 = "Essentiels 3 jours"
@@ -57,43 +57,54 @@ def main():
         assert col_name == COLUMN_NAMES[index], "Expected 'column %s', got '%s'" % (
             COLUMN_NAMES[index], col_name)
     print("Columns OK")
-    with open('utility_files/template_umap.umap') as umap_template_file:
-        umap_data = json.load(umap_template_file)
 
-        for row_nb, row in enumerate(values[1:]):
-            if row_nb % 50 == 0:
-                print("handling row %d" % row_nb)
-            if len(row) >= 22:
-                row = row + ["" for i in range(len(COLUMN_NAMES) - len(row))]
-                adress = row[5]
-                coords = geoloc.get(adress)
-                latitude_search = re.search(REGEX_LAT, adress)
-                longitude_search = re.search(REGEX_LON, adress)
-                if latitude_search and longitude_search:
-                    coords = [float(longitude_search.group("lon")), float(latitude_search.group("lat"))]
-                if not coords:
-                    geolocator = Nominatim(user_agent="pleniere_at_2020")
-                    location = geolocator.geocode(adress)
-                    sleep(1.5)
-                    if location:
-                        coords = [location.longitude, location.latitude]
-                        geoloc[adress] = coords
-                        with open('utility_files/geoloc_adresses.json', 'w') as saved_geoloc_file:
-                            json.dump(geoloc, saved_geoloc_file)
-                if not coords:
-                    coords = [-4.6561 + random.random() * 1.5, 49.1150]
-                elif coords[0] < -5.4756155192 or coords[0] > 3.7230954436 or coords[1] > 50.359718657 or coords[1] < 46.6907131609:
-                    coords = [-4.6561 + random.random() * 1.5, 49.2]
-                emoji = row[26].split(" ")[0] if row[26] else ""
-                url_image = row[27]
-                if row[21] in [DAYS_3, DAYS_7, BONUS]:
-                    layer_number = {
-                        DAYS_3: 2,
-                        DAYS_7: 1,
-                        BONUS: 0,
-                    }[row[21]]
-                    hashtag = "#" + row[25].replace("#", "") + "\n" if row[25] else ""
-                    text = """%s%s**Pourquoi il faut absolument y passer ?**
+    days_3_features = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    bonus_features = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    days_7_features = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for row_nb, row in enumerate(values[1:]):
+        if row_nb % 50 == 0:
+            print("handling row %d" % row_nb)
+        if len(row) >= 22:
+            row = row + ["" for i in range(len(COLUMN_NAMES) - len(row))]
+            adress = row[5]
+            coords = geoloc.get(adress)
+            latitude_search = re.search(REGEX_LAT, adress)
+            longitude_search = re.search(REGEX_LON, adress)
+            if latitude_search and longitude_search:
+                coords = [float(longitude_search.group("lon")), float(latitude_search.group("lat"))]
+            if not coords:
+                geolocator = Nominatim(user_agent="pleniere_at_2020")
+                location = geolocator.geocode(adress)
+                sleep(1.5)
+                if location:
+                    coords = [location.longitude, location.latitude]
+                    geoloc[adress] = coords
+                    with open('utility_files/geoloc_adresses.json', 'w') as saved_geoloc_file:
+                        json.dump(geoloc, saved_geoloc_file)
+            if not coords:
+                coords = [-4.6561 + random.random() * 1.5, 49.1150]
+            elif coords[0] < -5.4756155192 or coords[0] > 3.7230954436 or coords[1] > 50.359718657 or coords[1] < 46.6907131609:
+                coords = [-4.6561 + random.random() * 1.5, 49.2]
+            emoji = row[26].split(" ")[0] if row[26] else ""
+            url_image = row[27]
+            if row[21] in [DAYS_3, DAYS_7, BONUS]:
+                feature_collection = {
+                    DAYS_3: days_3_features,
+                    DAYS_7: days_7_features,
+                    BONUS: bonus_features,
+                }[row[21]]
+                hashtag = "#" + row[25].replace("#", "") + "\n" if row[25] else ""
+                text = """%s%s**Pourquoi il faut absolument y passer ?**
 1️⃣ %s
 2️⃣ %s
 3️⃣ %s
@@ -103,25 +114,29 @@ def main():
 
 **Description**
 %s
-                    """ % ("{{%s}}\n" % url_image if url_image else "", hashtag, row[22] or "A remplir !", row[23] or "A remplir !", row[24] or "A remplir !", row[18] if row[18] else "A remplir !", row[19] if row[19] else "A remplir !", row[11])
+                """ % ("{{%s}}\n" % url_image if url_image else "", hashtag, row[22] or "A remplir !", row[23] or "A remplir !", row[24] or "A remplir !", row[18] if row[18] else "A remplir !", row[19] if row[19] else "A remplir !", row[11])
 
-                    umap_data["layers"][layer_number]["features"].append({
-                        "type": "Feature",
-                        "properties": {
-                            "description": text,
-                            "name": emoji + " " + row[3],
-                            "_umap_options": {
-                                "iconUrl": emoji
-                            }
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": coords
+                feature_collection["features"].append({
+                    "type": "Feature",
+                    "properties": {
+                        "description": text,
+                        "name": emoji + " " + row[3],
+                        "_umap_options": {
+                            "iconUrl": emoji
                         }
-                    })
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": coords
+                    }
+                })
 
-    with open('output.umap', 'w') as outfile:
-        json.dump(umap_data, outfile)
+    with open('days_3_features.umap', 'w') as outfile:
+        json.dump(days_3_features, outfile)
+    with open('days_7_features.umap', 'w') as outfile:
+        json.dump(days_7_features, outfile)
+    with open('bonus_features.umap', 'w') as outfile:
+        json.dump(bonus_features, outfile)
 
 
 # TODOs
